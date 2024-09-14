e063b7bc53208fbc03aa2a66937c3c833971668d
a <perromueble@outlook.com>
2024-09-14 14:16:47 -0600

    Added acorn collectible and acorn amount HUD

---
+++ scenes/collectibles/acorn_amount_hud.gd
@@ -0,0 +1,14 @@
+extends PanelContainer
+
+@export_group("Nodes Accessed in Script")
+@export var _amount_label: Label;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	CollectiblesController.acorn_amount_changed.connect(_on_acorn_amount_changed);
+
+
+
+
+func _on_acorn_amount_changed(__new_amount: int) -> void:
+	_amount_label.text = str(__new_amount);
