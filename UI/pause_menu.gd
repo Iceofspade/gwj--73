d257e98c11d13ce1486ce2a7c0453196774b1a3e
Trevon <64873520+swirlybone@users.noreply.github.com>
2024-09-16 22:04:08 -0400

    Update pause_menu.gd

--- UI/pause_menu.gd
+++ UI/pause_menu.gd
@@ -2,13 +2,17 @@ extends Control
 
 @onready var main = $"../../" #two above reference
 
-
+func _process(delta: float) -> void:
+	#if _on_restart_pressed():
+		#get_tree().reload_current_scene()
+	pass
 func _on_resume_pressed() -> void:
 	main.pauseMenu()
 
 
-func _on_restart_pressed() -> void:
-	pass # Replace with function body.
+func _on_restart_pressed():
+	#get_tree().reload_scene()
+	pass
 
 
 func _on_quit_pressed() -> void:
