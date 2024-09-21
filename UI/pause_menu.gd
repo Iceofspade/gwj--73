19a22352d91dc1a49610d06032aa014e6e5d4450
Trevon <64873520+swirlybone@users.noreply.github.com>
2024-09-21 18:30:26 -0400

    PauseMenuBeta!

--- UI/pause_menu.gd
+++ UI/pause_menu.gd
@@ -1,19 +1,41 @@
 extends Control
 
-@onready var main = $"../../" #two above reference
+func _ready():
+	$AnimationPlayer.play("RESET")
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
 
-func _process(delta: float) -> void:
+func Esc():
+	if Input.is_action_just_pressed("pause") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("pause") and get_tree().paused:
+		resume()
+#@onready var main = $"../../" #two above reference
+
+#func _process(delta: float) -> void:
 	#if _on_restart_pressed():
 		#get_tree().reload_current_scene()
+	#pass
+func _on_resume_pressed():
+	#main.pauseMenu() <-Alpha
+	resume()
 	pass
-func _on_resume_pressed() -> void:
-	main.pauseMenu()
 
 
 func _on_restart_pressed():
-	#get_tree().reload_scene()
+	#get_tree().reload_scene() <-Alpha
+	resume()
+	get_tree().reload_current_scene()
+	
 	pass
 
 
-func _on_quit_pressed() -> void:
+func _on_quit_pressed():
 	get_tree().quit()
+	
+func _process(delta):
+	Esc()
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
 
-@onready var pause_menu = $HUDCanvasLayer/PauseMenu
+#@onready var pause_menu = $HUDCanvasLayer/PauseMenu
 
-var paused = false
+#var paused = false
 # Called when the node enters the scene tree for the first time.
 
 
@@ -13,21 +13,21 @@ var paused = false
 	#pass
 
 func _process(delta):
-	if Input.is_action_just_pressed("pause"):
-		pauseMenu()
-		
-func pauseMenu():
-	if paused:
+	#if Input.is_action_just_pressed("pause"):
+		#pauseMenu()
+		pass
+#func pauseMenu():
+	#if paused:
 		#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-		pause_menu.hide()
-		#Engine.time_scale = 1
-		get_tree().paused = false
-	else:
-		#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		pause_menu.show()
-		#Engine.time_scale = 0
-		get_tree().paused = true
+		#pause_menu.hide()
+		##Engine.time_scale = 1
+		#get_tree().paused = false
+	#else:
+		##Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		#pause_menu.show()
+		##Engine.time_scale = 0
+		#get_tree().paused = true
 		
 		
-	paused = !paused
+	#paused = !paused
 	
