9983974ae7b17bbb2f27a43158ec94a9c85e8dde
Trevon <64873520+swirlybone@users.noreply.github.com>
2024-09-22 14:09:52 -0400

    Platformer!

---
+++ platform.gd
@@ -0,0 +1,15 @@
+extends StaticBody2D
+@onready var collision = $CollisionShape2D
+@onready var area = $Area2D
+
+
+#func _unhandled_input(event: InputEvent) -> void:
+	#if(event.is_action_pressed("move_down"))
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
