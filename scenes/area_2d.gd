0eeb229cb92baec07af89728bccaa0f7c22d860f
Trevon <64873520+swirlybone@users.noreply.github.com>
2024-09-22 23:12:08 -0400

    beta

---
+++ scenes/area_2d.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+var entered = false
+
+func _on_Area2D_body_entered(body: PhysicsBody2D):
+	entered = true
+	
+func _on_Area2D_body_exited(body):
+	entered = false
+	
+func _process(delta):
+	if entered == true:
+		get_tree().change_scene("res://scenes/EndGameScene.tscn")
